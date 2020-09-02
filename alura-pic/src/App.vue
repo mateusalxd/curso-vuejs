1ec21b6f6ef4b5db404e4d034796cbeb68616ceb
mateusalxd <mateusalxd@gmail.com>
2020-09-02 15:11:45 -0300

    Recuperar dados da api

--- alura-pic/src/App.vue
+++ alura-pic/src/App.vue
@@ -10,23 +10,19 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
       titulo: "Alurapic",
-      fotos: [
-        {
-          url:
-            "http://data.biovet.com.br/file/2018/10/29/H104520-F00000-V006-2000x0.jpeg",
-          titulo: "cachorro",
-        },
-        {
-          url:
-            "http://data.biovet.com.br/file/2018/10/29/H104520-F00000-V006-2000x0.jpeg",
-          titulo: "Cachorrão",
-        },
-      ],
+      fotos: [],
     };
+  },
+  created() {
+    axios.get("http://localhost:3000/v1/fotos").then(
+      (fotos) => (this.fotos = fotos.data),
+      (err) => console.log(err)
+    );
   },
 };
 </script>
